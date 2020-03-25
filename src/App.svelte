52a95843811d5f7de57bf5a2098a0a5a04251abe
Michael Mancini <thejazzymichael@gmail.com>
2020-03-25 14:54:28 -0400

    add wired button for github source link, remove overflow-x, clean up css

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
 import { onMount } from 'svelte';
 import { WiredCard } from 'wired-card';
 import { WiredLink } from 'wired-link';
+import { WiredButton } from 'wired-button';
 import LineChart from './LineChart.svelte';
 
 const url = 'https://pomber.github.io/covid19/timeseries.json';
@@ -45,135 +46,99 @@ onMount(async () => {
 	}, 0));
 });
 
-
 </script>
 
-<div class="header">
-	<h2 style="color: #ff3e00; font-weight: 400;">🍺 CARONA VIRUS 🤢</h2>
-	
-	{#if lastUpdate}
-		<p style="font-size: 0.9em">Last Updated: {lastUpdate}</p>
-	{:else}
-		<p style="font-size: 0.9em">   </p>
-	{/if}
+<svelte:head>
+	<style>
+		html, body {
+			overflow-x: hidden;
+		}
+	</style>
+</svelte:head>
 
-	<wired-card elevation="3" fill="#1a83e6" style="padding: 2em">
+<wired-card fill="#fff1f4" class="header-container">
+	<div class="header-content">
+		<h2 style="color: #e48f73; font-weight: 400;">🍺 CARONA VIRUS 🤢</h2>
+
+		<span style="width: 1em"></span>
+		
+		{#if lastUpdate}
+			<p style="font-size: 0.9em;">Last Updated: {lastUpdate}</p>
+		{:else}
+			<p style="font-size: 0.9em">   </p>
+		{/if}
+	</div>
+	<wired-card class="header-divider" elevation="2"></wired-card>
+</wired-card>
+
+<div class="stat-cards">
+	<wired-card elevation="3" fill="#1a83e6" style="padding: 2em; margin: 0.2em;">
 		<span style="color: white">{totalConfirmed} Cases</span>
 	</wired-card>
 
-	<wired-card elevation="3" fill="#ff3e00" style="padding: 2em">
+	<wired-card elevation="3" fill="#ff3e00" style="padding: 2em; margin: 0.2em;">
 		<span style="color: white">{totalDeaths} Deaths</span>
 	</wired-card>
 </div>
 
 <LineChart countries={data}></LineChart>
 
-<wired-card elevation="3" style="margin: 4em auto; display: block; text-align: center;">
-	<p>*Data comes from the Novel Coronavirus COVID-19 Data Repository by Johns Hopkins Center for Systems Science and Engineering. It is then cleansed and converted to JSON using an open-source project developed by <wired-link href="https://pomb.us/" target="_blank">"Pomber"</wired-link> before being interpretted by this Svelte App! Source code coming soon.</p>
+<wired-card class="disclaimer-card">
+	<p>Data is from the Novel Coronavirus COVID-19 Data Repository by Johns Hopkins Center for Systems Science and Engineering. It is converted to an easily accessible JSON document and updated daily by <wired-link href="https://pomb.us/" target="_blank">"Pomber"</wired-link> before being analyzed here on the site.</p>
 	<p><wired-link href="https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6" target="_blank">Click Here</wired-link> to view the official Johns Hopkins Dashboard.</p>
 </wired-card>
 
+<wired-button elevation="3" class="source-button">
+	<a
+		style="display: flex; align-items: center; padding: 0.5em; color: #3934c1;"
+		href="https://github.com/Jappzy/carona-balona"
+		target="_blank">
+		<svg role="img" height="40" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub icon</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
+		<span style="margin-left: 1em">View Source</span>
+	</a>
+</wired-button>
+
 <style>
-	.header {
-		text-align: center;
-		margin: 1em auto 3em;
-	}
+.header-container {
+	overflow: hidden;
+	display: block;
+	margin: -16px;
+	width: calc(100% + 32px);
+	padding-bottom: 0;
+	box-sizing: border-box;
+}
 
-	.chart-filters {
- 		display: flex;
-		justify-content: space-around;
-		align-items: center;
-		flex-wrap: wrap;
-		width: 95%;
-		max-width: 666px;
-		margin: 1em auto;
-	}
+.header-content {
+	text-align: center;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-wrap: wrap;
+	overflow: hidden;
+}
 
-	.chart-card {
-		width: 95%;
-		margin: auto;
-		display: block;
-		box-sizing: border-box;
-	}
+.header-divider {
+	color: #e48f73;
+	width: 100%;
+	margin-left: -8px;
+	margin-right: -8px;
+}
 
-	/* only chart-specific styles below */
-	.chart {
-		height: 400px;
-		padding: 1em 0 1.5em 36px;
-	}
+.stat-cards {
+	display: flex;
+	justify-content: center;
+	margin: 2em auto;
+}
 
-	.grid-line {
-		position: relative;
-		display: block;
-	}
+.disclaimer-card {
+	margin: 4em auto 2em;
+	display: block;
+	text-align: center;
+}
 
-	.grid-line.horizontal {
-		width: calc(100% + 2em);
-		left: -2em;
-		border-bottom: 1px dashed #ccc;
-	}
-
-	.grid-line span {
-		position: absolute;
-		left: 0;
-		bottom: 2px;
-		font-family: sans-serif;
-		font-size: 14px;
-		color: #999;
-	}
-
-	.x-label {
-		position: absolute;
-		width: 4em;
-		left: -2em;
-		bottom: -22px;
-		font-family: sans-serif;
-		font-size: 14px;
-		color: #999;
-		text-align: center;
-	}
-
-	path.data {
-		stroke: rgba(0,0,0,0.2);
-		stroke-linejoin: round;
-		stroke-linecap: round;
-		stroke-width: 1px;
-		fill: none;
-	}
-
-	.highlight {
-		stroke: #ff3e00;
-		fill: none;
-		stroke-width: 2;
-	}
-
-	.annotation {
-		position: absolute;
-		white-space: nowrap;
-		bottom: 1em;
-		line-height: 1.2;
-		background-color: rgba(255,255,255,0.9);
-		padding: 0.2em 0.4em;
-		border-radius: 2px;
-	}
-
-	.annotation-point {
-		position: absolute;
-		width: 10px;
-		height: 10px;
-		background-color: #ff3e00;
-		border-radius: 50%;
-		transform: translate(-50%,-50%);
-	}
-
-	.annotation strong {
-		display: block;
-		font-size: 20px;
-	}
-
-	.annotation span {
-		display: block;
-		font-size: 14px;
-	}
-	/* only chart styles above */
+.source-button {
+	margin: 2em auto 4em;
+	display: block;
+	width: fit-content;
+}
 </style>
